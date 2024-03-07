b1481514391b581efb7d6db9390cdf98798182c4
Alex Ov <alexov@swisscows.email>
2024-03-07 17:57:51 +0200

    Tailor api specs to the current needs

--- src/test/java/specs/GenApiSpec.java
+++ src/test/java/specs/GenApiSpec.java
@@ -4,21 +4,18 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.BODY;
-import static io.restassured.filter.log.LogDetail.STATUS;
-import static io.restassured.http.ContentType.JSON;
 import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.ALL;
+import static io.restassured.http.ContentType.JSON;
 
 public class GenApiSpec {
-    public static RequestSpecification genRequestSpec = with()
+    public static RequestSpecification loggingRequestSpec = with()
             .filter(withCustomTemplates())
-            .log().uri()
-            .log().body()
+            .log().all()
             .contentType(JSON);
 
-    public static ResponseSpecification genResponseSpec = new ResponseSpecBuilder()
-            .log(STATUS)
-            .log(BODY)
+    public static ResponseSpecification loggingResponseSpec = new ResponseSpecBuilder()
+            .log(ALL)
             .build();
 }
